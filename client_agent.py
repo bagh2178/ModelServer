cc9deca2c0a502222ff734e9d5692ba3d218553a
Hang Yin <64118131+bagh2178@users.noreply.github.com>
2024-11-27 10:02:39 +0800

    Update client_agent.py

--- client_agent.py
+++ client_agent.py
@@ -3,7 +3,7 @@ from .tcp import TCPClient
 
 class Agent_Client():
     def __init__(self):
-        self.tcpclient = TCPClient('127.0.0.1', 30083)
+        self.tcpclient = TCPClient('127.0.0.1', 7002)
 
     def __call__(self, prompt):
         response = self.tcpclient.send_and_receive_data(prompt)
