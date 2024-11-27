28dfa1faef8dac72a2d22920c5791531c822494a
Hang Yin <64118131+bagh2178@users.noreply.github.com>
2024-11-27 10:11:27 +0800

    Update start_llm.py

--- start_llm.py
+++ start_llm.py
@@ -4,7 +4,7 @@ script_path = os.path.realpath(__file__)
 script_dir = os.path.dirname(script_path)
 parent_dir = os.path.dirname(script_dir)
 sys.path.append(parent_dir)
-from ModelServer.llm_server import LLM_Server
+from ModelServer.server.llm import LLM_Server
 
 
 llm_server = LLM_Server()
