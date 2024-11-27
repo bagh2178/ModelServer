ea0f8d51342a4cd35a70fd4e4a624e135d863d89
Hang Yin <64118131+bagh2178@users.noreply.github.com>
2024-11-27 10:11:39 +0800

    Update start_vlm.py

--- start_vlm.py
+++ start_vlm.py
@@ -4,7 +4,7 @@ script_path = os.path.realpath(__file__)
 script_dir = os.path.dirname(script_path)
 parent_dir = os.path.dirname(script_dir)
 sys.path.append(parent_dir)
-from ModelServer.vlm_server import VLM_Server
+from ModelServer.server.vlm import VLM_Server
 
 
 vlm_server = VLM_Server()
