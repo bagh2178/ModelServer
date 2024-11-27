96d01cc19d787cfc83b2853a3be8dc6980035ed2
Hang Yin <64118131+bagh2178@users.noreply.github.com>
2024-11-27 10:01:57 +0800

    Update server_agent.py

--- server_agent.py
+++ server_agent.py
@@ -5,6 +5,6 @@ from .tcp import TCPServer
 class Agent_Server():
     def __init__(self):
         self.agent = Agent()
-        self.tcpserver = TCPServer('0.0.0.0', 30083)
+        self.tcpserver = TCPServer('0.0.0.0', 7002)
         self.tcpserver.set_processor(self.agent)
         self.tcpserver.start()
