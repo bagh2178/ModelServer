c5a9933d490c057246e54ebcaf65e01e687f76de
TL <15104291162@163.com>
2024-12-22 23:33:49 +0800

    update tcpserver.py

--- tcp/tcpserver.py
+++ tcp/tcpserver.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 import struct
 import pickle
@@ -13,6 +14,16 @@ class TCPServer:
     def set_processor(self, processor):
         self.processor = processor
 
+    def recv_and_senf(self, conn, addr):
+        print(f"Connected by {addr}")
+        data_length = struct.unpack('>I', conn.recv(4))[0]
+        data = receive_data(conn, data_length)
+        received_data = pickle.loads(data)
+        print("Received data:", received_data)
+        processed_data = self.processor(received_data)
+        encoded_data = pickle.dumps(processed_data)
+        conn.sendall(struct.pack('>I', len(encoded_data)) + encoded_data)
+
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             # Allow the server to bind to an address that is in TIME_WAIT state.
@@ -24,15 +35,12 @@ class TCPServer:
                 print(f"\033[92mREADY\033[0m")
                 conn, addr = s.accept()
                 with conn:
-                    # try:
-                        print(f"Connected by {addr}")
-                        data_length = struct.unpack('>I', conn.recv(4))[0]
-                        data = receive_data(conn, data_length)
-                        received_data = pickle.loads(data)
-                        print("Received data:", received_data)
-                        processed_data = self.processor(received_data)
-                        encoded_data = pickle.dumps(processed_data)
-                        conn.sendall(struct.pack('>I', len(encoded_data)) + encoded_data)
-                    # except Exception as e:
-                        # print(f"An error occurred: {e}")
+                    is_debugging = sys.gettrace() is not None
+                    if is_debugging:
+                        self.recv_and_senf(conn, addr)
+                    else:
+                        try:
+                            self.recv_and_senf(conn, addr)
+                        except Exception as e:
+                            print(f"An error occurred: {e}")
 
